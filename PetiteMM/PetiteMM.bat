364201d6a375b02b2a9dbcc07208df8f20396462
loveemu <loveemu@gmail.com>
2013-09-02 21:40:40 +0900

    PetiteMM: Update batch, I hate MS-DOS batch.

--- PetiteMM/PetiteMM.bat
+++ PetiteMM/PetiteMM.bat
@@ -1,7 +1,7 @@
 @setlocal
-@set PMMOPTS=
+@set PMMOPTS=""
 @if "%~1"=="" @goto usage
-@for %%a in (%*) do @java -jar %~dp0PetiteMM.jar %PMMOPTS% "%%~fa"
+@for %%a in (%*) do @java -jar %~dp0PetiteMM.jar "%PMMOPTS%" "%%~fa"
 @goto heaven
 :usage
 @java -jar %~dp0PetiteMM.jar
