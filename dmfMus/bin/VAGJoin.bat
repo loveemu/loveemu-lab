fb3c99eb5b3b690586803b170b11740bc7cf83cd
loveemu <loveemu@gmail.com>
2013-12-21 17:05:07 +0900

    dmfMus: add small toy, VAGJoin.bat

---
+++ dmfMus/bin/VAGJoin.bat
@@ -0,0 +1,2 @@
+@rem This batch might be useful if you going to use "Tool to search VH/VBs from single file", such as PGconv.
+@for %%a in (*.vh) do @copy /b "%%~dpna.vh" + "%%~dpna.vb" "%%~dpna.vag"
