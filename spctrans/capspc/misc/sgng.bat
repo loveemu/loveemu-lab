d3820ac4491bf46147fdce3d1d81abe4cb4de6ab
loveemu <loveemu@gmail.com>
2014-06-22 12:54:28 +0900

    capspc: Maintain toys.

--- spctrans/capspc/misc/sgng.bat
+++ spctrans/capspc/misc/sgng.bat
@@ -1,2 +1,5 @@
-@copy /B sgng-01.spc sgng.spc
-capspc --norel --song 1 --count 0x16 --loop 2 --gs --patchfix sgng_gs.lst sgng.spc sgng.mid sgng.html 2>stderr.txt
+@rem @copy /B sgng-01.spc sgng.spc
+@rem capspc --norel --song 1 --count 0x16 --loop 2 --gs --patchfix sgng_gs.lst sgng.spc sgng.mid sgng.html 2>stderr.txt
+
+@echo.>stderr.txt
+@for %%a in (sgng-*.spc) do capspc --loop 2 --gs --patchfix sgng_gs.lst %%a %%~na.mid %%~na.html 2>>stderr.txt
