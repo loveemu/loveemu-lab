8525d57431ce809c1bb2cadbb04b9ab5233db3c9
loveemu <loveemu@gmail.com>
2015-04-08 21:09:38 +0900

    Add preload logic to Madou Monogatari SNSF driver, whole parameter redesign

--- spctrans/compspc/dis/madou-snsf.asm
+++ spctrans/compspc/dis/madou-snsf.asm
@@ -48,28 +48,19 @@
 .EMPTYFILL $FF
 
 .DEFINE PARAM_SONG = $808000
-	.dw	$0000
-.DEFINE PARAM_SONG_2 = $808002
-	.dw	$0000
-.DEFINE PARAM_RESERVED_1 = $808004
-	.dw	$0000
-.DEFINE PARAM_RESERVED_2 = $808006
-	.dw	$0000
+	.db	$00
+.DEFINE PARAM_SONG_PRELOAD = $808001
+	.db	$00
+.DEFINE PARAM_SONG_TYPE = $808002
+	.db	$00
+.DEFINE PARAM_RESERVED = $808003
+	.db	$00
 
 ; setup routine derived from the original setup ($80/8000)
 Start:
 	sei
 	clc
 	xce
-
-;	sep	#$20
-;	rep	#$10
-;	stz	$420B
-;	stz	$420C
-;	lda	#$8F
-;	sta	$2100
-;	lda	#$00
-;	sta	$4200
 
 	; zero memory ($0000-$1FFF)
 	rep	#$30
@@ -78,19 +69,6 @@ Start:
 	stz	$00
 	lda	#$1ffd
 	mvn	$00,$00
-
-;	ldx #$0000
-;	ldy #$0001
-;	lda #$0000
-;	sta $7E0000
-;	lda #$FFFE
-;	mvn $7E,$7E
-;	ldx #$0000
-;	ldy #$0001
-;	lda #$0000
-;	sta $7F0000
-;	lda #$FFFE
-;	mvn $7F,$7F
 
 	sep	#$20
 	phk
@@ -124,38 +102,6 @@ Start:
 	sta	$4200
 	cli
 
-;	sep	#$20
-;	jsr	$8d2f
-;	ldx	#$1fff
-;	txs
-
-;	phk
-;	plb
-;	rep	#$20
-;	lda	#$cf6a
-;	sta	$01
-;	stz	$62
-;	stz	$5e
-;	stz	$60
-;	stz	$420b
-;	sep	#$20
-;	lda	#$01
-;	sta	$0d00
-;	lda	#$81
-;	sta	$0c84
-;	lda	#$81
-;	sta	$4200
-;	cli
-;	jsl	$02ede3
-;	jsr	$8b92
-;	jsr	$8b92
-;	lda	$0c5b
-;	inc	a
-;	sta	$0c5b
-;	cmp	#$0f
-;	bne	$8088
-;	jsl	$02eef9
-
 	; transfer SPC program
 	jsr	$e6ad
 
@@ -177,42 +123,45 @@ Start:
 	lda	#$01
 	sta	$5d
 
+	; check preload request
+	lda	PARAM_SONG_PRELOAD
+	bpl	loc_PlaySound
+
+loc_PreloadBGM:
+	; request BGM playback
+	and	#$7f
+	jsl	$80ebfa
+;	jsl	$80ec44
+
+	; nullify the playback
+	stz	$1c6d
+
+loc_WaitForPreload
+	; wait for data transfer
+	lda	$1c6c
+	bne	loc_WaitForPreload
+
 loc_PlaySound:
-	rep #$20
-	lda	PARAM_SONG_2
-	bmi	loc_PlaySFX
-
-loc_PlayBGMPreload:
+	lda	PARAM_SONG_TYPE
 	beq	loc_PlayBGM
-;	wai
-;	wai
-;	wai
-;	wai
-
-	sep #$20
-	jsl	$80ec44
+	bpl	loc_PlaySFX
+	bmi	loc_PlayVoice
 
 loc_PlayBGM:
 	lda	PARAM_SONG
-	sep #$20
 	jsl	$80ebfa
 ;	jsl	$80ec44
 
 	bra loc_MainLoop
 
 loc_PlaySFX:
-	and	#$7f
-	beq	loc_PlayVoice
-
 	lda	PARAM_SONG
-	sep #$20
 	jsl	$80e891
 
 	bra loc_MainLoop
 
 loc_PlayVoice:
 	lda	PARAM_SONG
-	sep #$20
 	jsl	$80ebb0
 
 loc_MainLoop:
